4ca248bc15853f9cc8d47e1494e928380dd37ee8
mustafa-keles <70661368+mustafa-keles@users.noreply.github.com>
2022-11-08 16:28:48 -0500

    test1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learnnn React
+          Learn React
         </a>
       </header>
     </div>
