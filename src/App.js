710f68c8b6a21b5f8e7aaafdc9a3cae164011fdb
mustafa-keles <70661368+mustafa-keles@users.noreply.github.com>
2022-11-08 19:24:29 -0500

    text edit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <code>This is my sample react project</code>
         </p>
         <a
           className="App-link"
